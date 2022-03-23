846199ee4c5355afd2e51a3d800d4fffa5f58c96
GeniuStone <dugustlr@naver.com>
2022-03-23 20:16:38 +0900

    2022-03-23 slide update

--- js/slide.js
+++ js/slide.js
@@ -1,114 +1,109 @@
 //<![CDATA[
-    $(document).ready(function(){
+$(document).ready(function () {
+  let $slider = $(".slider");
+  let count = 0;
 
-        var $slideNum = 0;
-        
-        $width = $("#slides").width();
-        $(window).resize(function(){
-            $width = $("#slides").width();
-        });
-        // 사용자의 창 사이즈가 변할 때마다 슬라이드 사이즈 저장 (슬라이드 엇나감 방지)
+  // 기본적으로 모든 슬라이드의 opacity는 0
+  let $firstSlide = $slider
+    .find("li")
+    .first()
+    .stop(true)
+    .animate({ opacity: 1 }, 200);
 
-        $(".next").click(function(){
-            $slideNum++;
-            if($slideNum>2){
-                $slideNum = 0;
-            }// $slideNum이 2보다 커지면 0으로 (다시 처음으로)
-            if($slideNum == 0){
-                $(".slide_smr h2, .slide_smr p, .slide_smr a").css("opacity","0");    
-            }// $slideNum이 0이면 (다시 처음으로 돌아올 때) 텍스트들 opacity : 0
-            $(".slide_container").stop().animate({left: -$width*$slideNum},600,function(){
-                $(".slide_smr h2").css("animation","movement 1s ease-in-out forwards");
-                $(".slide_smr p").css("animation","movement 1.3s ease-in-out forwards");
-                $(".slide_smr a").css("animation","movement 1.6s ease-in-out forwards");
-                // 슬라이드 이동 후 텍스트들에 애니메이션 css 부여
-                $(".bullet img").attr("src","./images/slide_image/state_out.png");
-                $(".bullet img").eq($slideNum).attr("src","./images/slide_image/state_over_black.png");
-            });// 초기화 후 해당하는 $slideNum의 인덱스를 가진 요소만 속성 변경
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("animation","");
-        });// 텍스트에 부여한 애니메이션 삭제
+  /* 이전 슬라이드 함수 */
+  function PrevSlide() {
+    // 마지막 슬라이드 맨 앞으로 보내기
+    let $lastSlide = $slider.find("li").last().prependTo($slider);
+    // 밀려난 두번째는 가리기
+    let $secondSlide = $slider
+      .find("li")
+      .eq(1)
+      .stop(true)
+      .animate({ opacity: 0 }, 400);
+    // 마지막에서 맨 앞으로 온 슬라이드는 보이도록
+    $firstSlide = $slider
+      .find("li")
+      .first() //맨 처음 슬라이드 다시 구하기
+      .stop(true)
+      .animate({ opacity: 1 }, 400);
 
-        $(".prev").click(function(){
-            $slideNum--;
-            if($slideNum<0){
-                $slideNum = 2;
-            }// $slideNum이 0보다 작아지면 2로 (마지막 슬라이드로)
-            $(".slide_container").stop().animate({left: -$width*$slideNum},600,function(){
-                $(".slide_smr h2").css("animation","movement 1s ease-in-out forwards");
-                $(".slide_smr p").css("animation","movement 1.3s ease-in-out forwards");
-                $(".slide_smr a").css("animation","movement 1.6s ease-in-out forwards");
-                // 슬라이드 이동 후 텍스트들에 애니메이션 css 부여
-                $(".bullet img").attr("src","./images/slide_image/state_out.png");
-                $(".bullet img").eq($slideNum).attr("src","./images/slide_image/state_over_black.png");
-            });// 초기화 후 해당하는 $slideNum의 인덱스를 가진 요소만 속성 변경
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("animation","");
-        });// 텍스트에 부여한 애니메이션 삭제
-        //슬라이드 버튼 제어
+    count--;
+    if (count < 0) {
+      count = 2;
+    }
+    bulletSlide(count);
+  }
 
-        setInterval(function(){
-            $slideNum++;
-            if($slideNum>2){
-                $slideNum = 0;
-            }// $slideNum이 2보다 커지면 0으로 (다시 처음으로)
-            if($slideNum == 0){
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("opacity","0");    
-            }// $slideNum이 0이면 (다시 처음으로 돌아올 때) 텍스트들 opacity : 0
-            $(".slide_container").stop().animate({left: -$width*$slideNum},600,function(){
-                $(".slide_smr h2").css("animation","movement 1s ease-in-out forwards");
-                $(".slide_smr p").css("animation","movement 1.3s ease-in-out forwards");
-                $(".slide_smr a").css("animation","movement 1.6s ease-in-out forwards");
-                // 슬라이드 이동 후 텍스트들에 애니메이션 css 부여    
-                $(".bullet img").attr("src","./images/slide_image/state_out.png");
-                $(".bullet img").eq($slideNum).attr("src","./images/slide_image/state_over_black.png");
-            });// 초기화 후 해당하는 $slideNum의 인덱스를 가진 요소만 속성 변경
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("animation","");
-        }, 3000);// 텍스트에 부여한 애니메이션 삭제
-        //슬라이드 3초마다 자동 넘김
+  /* 다음 슬라이드 함수 */
+  function NextSlide() {
+    // 처음 슬라이드를 맨 뒤로 보내고
+    $firstSlide.appendTo($slider);
+    // 맨 뒤로 보낸 슬라이드 안 보이게
+    $lastSlide = $slider
+      .find("li")
+      .last()
+      .stop(true)
+      .animate({ opacity: 0 }, 400);
+    // 첫번째 슬라이드(원래는 두번째)는 보이게
+    $firstSlide = $slider
+      .find("li")
+      .first()
+      .stop(true)
+      .animate({ opacity: 1 }, 400);
 
-        $(".bullet_1").click(function(){
-            $slideNum = 0; // 클릭했을 때 $slideNum에 0을 저장
-            $(".slide_container").stop().animate({left: -$width*$slideNum},600,function(){
-                $(".slide_smr h2").css("animation","movement 1s ease-in-out forwards");
-                $(".slide_smr p").css("animation","movement 1.3s ease-in-out forwards");
-                $(".slide_smr a").css("animation","movement 1.6s ease-in-out forwards");
+    count++;
+    if (count > 2) {
+      count = 0;
+    }
+    bulletSlide(count);
+  }
 
-                $(".bullet img").attr("src","./images/slide_image/state_out.png");
-                $(".bullet img").eq($slideNum).attr("src","./images/slide_image/state_over_black.png");
-            });
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("animation","");
-        });// $slideNum이 0일 때의 동작 
+  /* 슬라이드 하단 페이지네이션 버튼 */
+  function bulletSlide(num) {
+    let $remainedSlide = $slider
+      .find("li")
+      .stop(true)
+      .animate({ opacity: 0 }, 400);
 
-        $(".bullet_2").click(function(){
-            $slideNum = 1; // 클릭했을 때 $slideNum에 1을 저장
-            $(".slide_container").stop().animate({left: -$width*$slideNum},600,function(){
-                $(".slide_smr h2").css("animation","movement 1s ease-in-out forwards");
-                $(".slide_smr p").css("animation","movement 1.3s ease-in-out forwards");
-                $(".slide_smr a").css("animation","movement 1.6s ease-in-out forwards");
+    let $selectedSlide = $slider
+      .find("li")
+      .eq(num)
+      .stop(true)
+      .animate({ opacity: 1 }, 400);
 
-                $(".bullet img").attr("src","./images/slide_image/state_out.png");
-                $(".bullet img").eq($slideNum).attr("src","./images/slide_image/state_over_black.png");
-            });
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("animation","");
-        });// $slideNum이 1일 때의 동작
-        
-        $(".bullet_3").click(function(){
-            $slideNum = 2; // 클릭했을 때 $slideNum에 2를 저장
-            $(".slide_container").stop().animate({left: -$width*$slideNum},600,function(){
-                $(".slide_smr h2").css("animation","movement 1s ease-in-out forwards");
-                $(".slide_smr p").css("animation","movement 1.3s ease-in-out forwards");
-                $(".slide_smr a").css("animation","movement 1.6s ease-in-out forwards");
+    $(".bullet img").attr("src", "./images/slide_image/state_out.png");
+    $(".bullet img")
+      .eq(num)
+      .attr("src", "./images/slide_image/state_over.png");
+  }
 
-                $(".bullet img").attr("src","./images/slide_image/state_out.png");
-                $(".bullet img").eq($slideNum).attr("src","./images/slide_image/state_over_black.png");
-            });
-            $(".slide_smr h2, .slide_smr p, .slide_smr a").css("animation","");
-        });// $slideNum이 2일 때의 동작
-        //슬라이드 목록 버튼 제어
+  $(".prev").on("click", function () {
+    PrevSlide();
+  });
 
+  $(".next").on("click", function () {
+    NextSlide();
+  });
 
+  $(".bullet_1").on("click", function () {
+    bulletSlide(0);
+  });
 
+  $(".bullet_2").on("click", function () {
+    bulletSlide(1);
+  });
 
+  $(".bullet_3").on("click", function () {
+    bulletSlide(2);
+  });
 
+  // 자동 슬라이드
+  let interval;
 
-    });
+  function startSlide() {
+    interval = setInterval(NextSlide, 5000);
+  }  
+
+  startSlide();
+});
 //]]>
